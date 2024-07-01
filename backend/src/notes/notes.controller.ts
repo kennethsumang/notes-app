45b70be3ee26c2a9acc466249dd6270c1dc804ef
Kenneth Sumang <dev@kennethsumang.com>
2024-07-01 21:22:10 +0800

    feat(backend): add jwt auth guard in create note endpoint

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
 import CreateNoteDto from './dto/create-note.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('notes')
 export class NotesController {
   constructor(private notesService: NotesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.CREATED)
   @Post('/')
-  async create(@Body() notesDto: CreateNoteDto) {
-
-  }
+  async create(@Body() notesDto: CreateNoteDto) {}
 }
