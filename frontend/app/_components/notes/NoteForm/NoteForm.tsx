e73a91e04caa68ef6497b612cb00bbb3256959be
Kenneth Sumang <dev@kennethsumang.com>
2024-08-06 11:21:05 +0800

    feat: save note frontend handling

--- frontend/app/_components/notes/NoteForm/NoteForm.tsx
+++ frontend/app/_components/notes/NoteForm/NoteForm.tsx
@@ -1,21 +1,58 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import classes from "./NoteForm.module.css";
-import { Button, TextInput } from "@mantine/core";
-import Editor from "../Editor/Editor";
-import { Note } from "@/app/_types/notes";
+import { useState } from 'react';
+import classes from './NoteForm.module.css';
+import { Button, TextInput } from '@mantine/core';
+import Editor from '../Editor/Editor';
+import { Note } from '@/app/_types/notes';
+import RequestLibrary from '@/app/_libraries/request.library';
+import { getCurrentDomain } from '@/app/_utils/http.util';
+import useNotification from '@/app/_hooks/useNotification';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   note?: Note;
 }
 
-const NoteForm: React.FC<Props> = function ({ note = null}) {
+const NoteForm: React.FC<Props> = function ({ note = null }) {
+  const notify = useNotification();
+  const router = useRouter();
   const [title, setTitle] = useState<string>(note?.title || '');
   const [content, setContent] = useState<string>(note?.content || '');
+  const parent = null;
 
-  function handleSaveNote() {
+  async function handleSaveNote() {
+    const result = await requestSave();
+    if (result.success === false) {
+      notify.error(result.message);
+      return;
+    }
 
+    const note = result.data as Note;
+    router.push(`/app/notes/${note.id}`);
+  }
+
+  async function requestSave() {
+    try {
+      const response = await RequestLibrary.request<{ data: Note }>(
+        `${getCurrentDomain()}/api/notes`,
+        {
+          method: 'POST',
+          data: { title, content, parent },
+        },
+      );
+
+      return {
+        success: true,
+        message: 'OK',
+        data: response.data,
+      };
+    } catch (e) {
+      return {
+        success: false,
+        message: (e as Error).message,
+      };
+    }
   }
 
   return (
@@ -37,6 +74,6 @@ const NoteForm: React.FC<Props> = function ({ note = null}) {
       </div>
     </div>
   );
-}
+};
 
 export default NoteForm;
--- frontend/app/api/notes/route.ts
+++ frontend/app/api/notes/route.ts
@@ -1,7 +1,11 @@
-import HttpException from "@/app/_exceptions/http.exception";
-import { getErrorResponse, getSuccessResponse } from "@/app/_helpers/api-response.helper";
-import { NextRequest, NextResponse } from "next/server";
-import { ValidationError, number, object, string } from "yup";
+import HttpException from '@/app/_exceptions/http.exception';
+import {
+  getErrorResponse,
+  getSuccessResponse,
+} from '@/app/_helpers/api-response.helper';
+import _ from 'lodash';
+import { NextRequest, NextResponse } from 'next/server';
+import { ValidationError, number, object, string } from 'yup';
 
 /**
  * POST request handler
@@ -13,17 +17,37 @@ export async function POST(request: NextRequest, response: NextResponse) {
     title: string().required(),
     content: string().required(),
     user_id: number().required(),
+    parent: string().optional(),
+    token: string().required(),
   });
 
   try {
     const validated = await schema.validate(body, { strict: true });
-    // sample data
-    return getSuccessResponse({
-      id: 'e13ae390-3400-4370-88c0-fd7e797e26c7',
-      user_id: 1,
-      title: 'New Note',
-      content: '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, World!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
+    const bodyData = _.omit(validated, ['token']);
+    const url = new URL(`${process.env.BACKEND_URL ?? ''}/notes`);
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(bodyData),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${validated.token}`,
+      },
     });
+
+    if (response.status === 200) {
+      const jsonResponse = await response.json();
+      return getSuccessResponse(jsonResponse);
+    }
+
+    throw new HttpException(response.status, response.statusText);
+    // sample data
+    // return getSuccessResponse({
+    //   id: 'e13ae390-3400-4370-88c0-fd7e797e26c7',
+    //   user_id: 1,
+    //   title: 'New Note',
+    //   content:
+    //     '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, World!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
+    // });
   } catch (e) {
     return getErrorResponse(e);
   }
@@ -34,39 +58,59 @@ export async function GET(request: NextRequest, response: NextResponse) {
   const user_id = Number(searchParams.get('user_id'));
   const schema = object({
     user_id: number().required(),
+    token: string().required(),
   });
   const params = {
-    user_id: user_id
+    user_id: user_id,
   };
 
   try {
     const validated = await schema.validate(params, { strict: true });
+    const url = new URL(`${process.env.BACKEND_URL ?? ''}/notes`);
+    const response = await fetch(url, {
+      method: 'GET',
+      body: JSON.stringify(validated),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${validated.token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      const jsonResponse = await response.json();
+      return getSuccessResponse(jsonResponse);
+    }
+
+    throw new HttpException(response.status, response.statusText);
     // sample data
-    return getSuccessResponse([
-      {
-        id: 'e13ae390-3400-4370-88c0-fd7e797e26c7',
-        user_id: 1,
-        title: 'New Note',
-        content: '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, World!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
-        parent: null,
-      },
-      {
-        id: '7879a672-afe2-4a74-8cd9-3a69d732598e',
-        user_id: 1,
-        title: 'New Note 2',
-        content: '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, React!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
-        parent: null,
-        children: [
-          {
-            id: 'a2a87da3-4084-4259-ab6e-516ff36ddff4',
-            user_id: 1,
-            title: 'New Child Note',
-            content: '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"This is a child note!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
-            parent: '7879a672-afe2-4a74-8cd9-3a69d732598e',
-          }
-        ]
-      },
-    ])
+    // return getSuccessResponse([
+    //   {
+    //     id: 'e13ae390-3400-4370-88c0-fd7e797e26c7',
+    //     user_id: 1,
+    //     title: 'New Note',
+    //     content:
+    //       '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, World!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
+    //     parent: null,
+    //   },
+    //   {
+    //     id: '7879a672-afe2-4a74-8cd9-3a69d732598e',
+    //     user_id: 1,
+    //     title: 'New Note 2',
+    //     content:
+    //       '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, React!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
+    //     parent: null,
+    //     children: [
+    //       {
+    //         id: 'a2a87da3-4084-4259-ab6e-516ff36ddff4',
+    //         user_id: 1,
+    //         title: 'New Child Note',
+    //         content:
+    //           '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"This is a child note!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
+    //         parent: '7879a672-afe2-4a74-8cd9-3a69d732598e',
+    //       },
+    //     ],
+    //   },
+    // ]);
   } catch (e) {
     return getErrorResponse(e);
   }
